5d8811cc50fa4d279996831f3b252f26b65c1fae
tjeerdhans <tjeerdhans@gmail.com>
2021-04-28 20:52:03 +0200

    autoformat

--- 2018/2b.hs
+++ 2018/2b.hs
@@ -1,6 +1,6 @@
 module Aoc2018_2b where
 
-import Data.List ( sort, intersect )
+import Data.List (intersect, sort)
 
 --2b
 -- pair programmed with Roland P.
@@ -12,29 +12,31 @@ import Data.List ( sort, intersect )
 stringdif1 :: Int -> String -> String -> Bool
 stringdif1 1 [] [] = True
 stringdif1 _ [] [] = False
-stringdif1 0 (x:xs) (y:ys)
-    | x /= y = stringdif1 1 xs ys
-    | otherwise = stringdif1 0 xs ys
-stringdif1 1 (x:xs) (y:ys)
-    | x /= y = False
-    | otherwise = stringdif1 1 xs ys
+stringdif1 0 (x : xs) (y : ys)
+  | x /= y = stringdif1 1 xs ys
+  | otherwise = stringdif1 0 xs ys
+stringdif1 1 (x : xs) (y : ys)
+  | x /= y = False
+  | otherwise = stringdif1 1 xs ys
 
 -- not used, used `intersect` instead.
 commonChars :: String -> String -> String
 commonChars _ [] = []
 commonChars [] _ = []
-commonChars (x:xs) (y:ys) = if x==y then x:commonChars xs ys else commonChars xs ys
+commonChars (x : xs) (y : ys)
+  | length xs /= length ys = []
+  | x == y = x : commonChars xs ys
+  | otherwise = commonChars xs ys
 
 -- Get the unique chars of the correct box id, leaving out the common chars.
 -- this assumes a sorted list
 getCorrectIdSetChars :: [String] -> Maybe String
 getCorrectIdSetChars [] = Nothing
-getCorrectIdSetChars (x:y:ys) = if stringdif1 0 x y then Just (x `intersect` y) else getCorrectIdSetChars (y:ys)
+getCorrectIdSetChars (x : y : ys) = if stringdif1 0 x y then Just (x `intersect` y) else getCorrectIdSetChars (y : ys)
 
 main :: IO ()
 main = do
-    fileText <- readFile "input2.txt"
-    let ids = lines fileText
-    let chars = getCorrectIdSetChars $ sort ids
-    print chars
-
+  fileText <- readFile "input2.txt"
+  let ids = lines fileText
+  let chars = getCorrectIdSetChars $ sort ids
+  print chars
