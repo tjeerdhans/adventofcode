6a76b8375444e02c46b4864e1bf3ff6340e51edc
tjeerdhans <tjeerdhans@gmail.com>
2021-12-11 21:54:56 +0100

    wip part 2

--- 2021/aoc2021/aoc2021/DayThree.cs
+++ 2021/aoc2021/aoc2021/DayThree.cs
@@ -2,16 +2,18 @@ namespace aoc2021;
 
 public class DayThree : Day
 {
+    private int[][] _bitCounts;
+
     public DayThree() : base(3, 198, 230)
     {
     }
 
     protected override long GetFirstAnswer(string[] set)
     {
-        var bitCounts = new int[set[0].Length][];
+        _bitCounts = new int[set[0].Length][];
         for (int i = 0; i < set[0].Length; i++)
         {
-            bitCounts[i] = new[] { 0, 0 };
+            _bitCounts[i] = new[] { 0, 0 };
         }
 
         foreach (var s in set)
@@ -20,18 +22,18 @@ public class DayThree : Day
             {
                 if (s[i] == '0')
                 {
-                    bitCounts[i][0]++;
+                    _bitCounts[i][0]++;
                 }
                 else
                 {
-                    bitCounts[i][1]++;
+                    _bitCounts[i][1]++;
                 }
             }
         }
 
-        var gammaRateString = new string(bitCounts.Select(c => c[0] > c[1] ? '0' : '1').ToArray());
+        var gammaRateString = new string(_bitCounts.Select(c => c[0] > c[1] ? '0' : '1').ToArray());
         var gammaRate = Convert.ToInt32(gammaRateString, 2);
-        var epsilonRateString = new string(bitCounts.Select(c => c[0] > c[1] ? '1' : '0').ToArray());
+        var epsilonRateString = new string(_bitCounts.Select(c => c[0] > c[1] ? '1' : '0').ToArray());
         var epsilonRate = Convert.ToInt32(epsilonRateString, 2);
 
         return gammaRate * epsilonRate;
@@ -45,6 +47,27 @@ public class DayThree : Day
     /// <returns></returns>
     protected override long GetSecondAnswer(string[] set)
     {
-        return 0;
+        var mostCommonBit = _bitCounts[0][1] >= _bitCounts[0][0] ? '1' : '0';
+        var leastCommonBit = mostCommonBit == '0' ? '1' : '0';
+        var ogrList = set.Where(s => s[0] == mostCommonBit).ToList(); // Oxygen Generator Rating
+        var csrList = set.Where(s => s[0] == leastCommonBit).ToList(); // CO2 Scrubber Rating
+        for (int i = 1; i < _bitCounts.Length; i++)
+        {
+            mostCommonBit = _bitCounts[i][1] >= _bitCounts[i][0] ? '1' : '0';
+            leastCommonBit = mostCommonBit == '0' ? '1' : '0';
+            if (ogrList.Count > 1)
+            {
+                ogrList = ogrList.Where(s => s[i] == mostCommonBit).ToList();
+            }
+
+            if (csrList.Count > 1)
+            {
+                csrList = csrList.Where(s => s[i] == leastCommonBit).ToList();
+            }
+        }
+
+        var ogr = Convert.ToInt32(ogrList[0], 2);
+        var csr = Convert.ToInt32(csrList[0], 2);
+        return ogr * csr;
     }
 }
