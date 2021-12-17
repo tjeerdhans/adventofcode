cbf0bc3aca1ed101df70a0c74ead9d1032fe2a69
tjeerdhans <tjeerdhans@gmail.com>
2021-12-17 21:03:04 +0100

    day 9 part work in progress

--- 2021/aoc2021/aoc2021/Day09.cs
+++ 2021/aoc2021/aoc2021/Day09.cs
@@ -11,8 +11,45 @@ public class Day09 : Day
     protected override long GetFirstAnswer(string[] set)
     {
         var heightMap = set.Select(s => s.Select(i => int.Parse(i.ToString())).ToArray()).ToArray();
-        
-        var riskLevelSum = 0;
+
+        var lowPoints = GetLowPoints(heightMap);
+        var riskLevelSum = lowPoints.Sum(p => heightMap[p.x][p.y] + 1);
+
+        return riskLevelSum;
+    }
+
+    protected override long GetSecondAnswer(string[] set)
+    {
+        var heightMap = set.Select(s => s.Select(i => int.Parse(i.ToString())).ToArray()).ToArray();
+
+        var lowPoints = GetLowPoints(heightMap);
+        var basinSizes = new List<int>();
+        foreach (var p in lowPoints)
+        {
+            basinSizes.Add(GetBasinSize(heightMap, p));
+        }
+
+        basinSizes.Sort();
+        return basinSizes.TakeLast(3).Aggregate((result, next) => result * next);
+    }
+
+    private static int GetBasinSize(int[][] heightMap, (int x, int y) lowPoint)
+    {
+        var result = 1;
+        for (int x = lowPoint.x; x < heightMap.Length; x++)
+        {
+            if (heightMap[x][lowPoint.y] < 9)
+            {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+    private static IEnumerable<(int x, int y)> GetLowPoints(int[][] heightMap)
+    {
+        var result = new List<(int x, int y)>();
         for (int x = 0; x < heightMap.Length; x++)
         {
             for (int y = 0; y < heightMap[x].Length; y++)
@@ -24,21 +61,21 @@ public class Day09 : Day
                     {
                         if (heightMap[1][0] > h && heightMap[0][1] > h)
                         {
-                            riskLevelSum += h + 1;
+                            result.Add((x, y));
                         }
                     }
                     else if (y == heightMap[x].Length - 1) // top right corner
                     {
                         if (heightMap[1][y] > h && heightMap[0][y - 1] > h)
                         {
-                            riskLevelSum += h + 1;
+                            result.Add((x, y));
                         }
                     }
                     else // top edge
                     {
                         if (heightMap[0][y - 1] > h && heightMap[1][y] > h && heightMap[0][y + 1] > h)
                         {
-                            riskLevelSum += h + 1;
+                            result.Add((x, y));
                         }
                     }
                 }
@@ -48,21 +85,21 @@ public class Day09 : Day
                     {
                         if (heightMap[x - 1][0] > h && heightMap[x][1] > h)
                         {
-                            riskLevelSum += h + 1;
+                            result.Add((x, y));
                         }
                     }
                     else if (y == heightMap[x].Length - 1) // bottom right corner
                     {
                         if (heightMap[x][y - 1] > h && heightMap[x - 1][y] > h)
                         {
-                            riskLevelSum += h + 1;
+                            result.Add((x, y));
                         }
                     }
                     else // bottom edge
                     {
                         if (heightMap[x][y - 1] > h && heightMap[x - 1][y] > h && heightMap[x][y + 1] > h)
                         {
-                            riskLevelSum += h + 1;
+                            result.Add((x, y));
                         }
                     }
                 }
@@ -72,14 +109,14 @@ public class Day09 : Day
                     {
                         if (heightMap[x - 1][y] > h && heightMap[x][y + 1] > h && heightMap[x + 1][y] > h)
                         {
-                            riskLevelSum += h + 1;
+                            result.Add((x, y));
                         }
                     }
                     else if (y == heightMap[x].Length - 1) // right edge
                     {
                         if (heightMap[x - 1][y] > h && heightMap[x][y - 1] > h && heightMap[x + 1][y] > h)
                         {
-                            riskLevelSum += h + 1;
+                            result.Add((x, y));
                         }
                     }
                     else // the rest
@@ -87,18 +124,13 @@ public class Day09 : Day
                         if (heightMap[x][y - 1] > h && heightMap[x - 1][y] > h && heightMap[x][y + 1] > h &&
                             heightMap[x + 1][y] > h)
                         {
-                            riskLevelSum += h + 1;
+                            result.Add((x, y));
                         }
                     }
                 }
             }
         }
 
-        return riskLevelSum;
-    }
-
-    protected override long GetSecondAnswer(string[] set)
-    {
-        return 0;
+        return result;
     }
 }
